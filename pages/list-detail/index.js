09d9d64ad0df9c9a367dfc2da1a86b24e4da7f85
xiangxinfei <1585115154@qq.com>
2019-11-28 16:04:56 +0800

    店的介绍

--- pages/list-detail/index.js
+++ pages/list-detail/index.js
@@ -2,12 +2,17 @@ import ApiListDetail from '../../api/list-detail'
 Page({
   data: {
     shops: {},
+    showData: false,
   },
   onLoad(options) {
     ApiListDetail.getDetail(options.id).then((res) => {
       this.setData({
         shops: res.data,
+        showData: true
       })
+      wx.setNavigationBarTitle({
+        title: res.data.name,
+      });
     })
   },
   /* 查看轮播图片 */
