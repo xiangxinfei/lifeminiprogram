57e2a27c76c1c87bcf10eccbd919de5af91779ec
xiangxinfei <1585115154@qq.com>
2019-11-28 22:44:34 +0800

    评论的图片及其预览功能

--- pages/list-detail/index.js
+++ pages/list-detail/index.js
@@ -3,9 +3,15 @@ Page({
   data: {
     shops: {},
     showData: false,
+    commontId:0,
   },
   onLoad(options) {
     ApiListDetail.getDetail(options.id).then((res) => {
+      res.data.comments.forEach((item) => {
+        item.images = item.images.map((value) => {
+          return value.replace('w.h', '100.100');
+        })
+      })
       this.setData({
         shops: res.data,
         showData: true
@@ -16,10 +22,23 @@ Page({
     })
   },
   /* 查看轮播图片 */
-  viewImage(e) {
+  previewImage(e) {
     wx.previewImage({
       urls: this.data.shops.images,
       current: e.target.dataset.src
     })
   },
+  // 利用事件捕获原理，父元素传递 data-index
+  deliverId(e) {
+    this.setData({
+      commontId: e.currentTarget.dataset.index
+    })
+  },
+  // 查看评论图片
+  viewImage(e) {
+    wx.previewImage({
+      urls: this.data.shops.comments[this.data.commontId].images,
+      current: e.target.dataset.src
+    })
+  }
 })
